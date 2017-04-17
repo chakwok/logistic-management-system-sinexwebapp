c6238c89d72a20ee402c1213b4443c482de2191d
chakwok <27881079+chakwok@users.noreply.github.com>
2017-04-17 17:39:05 +0800

    Improved design of shwoing the ShippingAccountId

--- SinExWebApp20265462/Controllers/ShipmentsController.cs
+++ SinExWebApp20265462/Controllers/ShipmentsController.cs
@@ -71,7 +71,7 @@ namespace SinExWebApp20265462.Controllers
                 ViewBag.CurrentShippingAccountId = ShippingAccountId;
                 shipmentSearch.Shipment.ShippingAccountId = ShippingAccountId.GetValueOrDefault();
                 shipmentSearch.Shipment.AccountType = db.ShippingAccounts.First(s => s.UserName == name).AccountType;
-                shipmentSearch.Shipment.DisplayedShippingAccountId = ShowShippingAccountId(ShippingAccountId.GetValueOrDefault());
+                ViewBag.DisplayedShippingAccountId = ShowShippingAccountId(ShippingAccountId.GetValueOrDefault());
             }
             else
             {
