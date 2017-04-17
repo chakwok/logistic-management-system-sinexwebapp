52566321572d83a3da030396b00d79145d956805
WONG Tak Kin <tkwongao@WIN2K.CS.UST.HK>
2017-04-17 21:28:35 +0800

    Manage account successful.

---
+++ SinExWebApp20265462/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SinExWebApp20265462.Models
+{
+    [Table("Address")]
+    public class Address
+    {
+        public virtual int AddressID { get; set; }
+        public virtual string address { get; set; }
+        [Required]
+        public virtual string nickname { get; set; }
+    }
+}
