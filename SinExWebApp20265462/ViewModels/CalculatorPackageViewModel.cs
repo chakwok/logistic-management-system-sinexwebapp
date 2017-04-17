03fb87b681bd1d2d10568d7188e6aff65448b95f
wingkeicheung <wkcheungah@connect.ust.hk>
2017-04-17 21:31:14 +0800

    Shipment cost calculator in progress.

---
+++ SinExWebApp20265462/ViewModels/CalculatorPackageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinExWebApp20265462.ViewModels
+{
+    public class CalculatorPackageViewModel
+    {
+        [Key]
+        public virtual int CalculatorPackageID { get; set; }
+        public virtual int PackageTypeSizeID { get; set; }
+        public virtual List<SelectListItem> PackageTypeSizes { get; set; }
+
+        public virtual float Weight { get; set; }
+
+        public virtual decimal PackageCost { get; set; }
+
+        public CalculatorShipmentViewModel Shipment { get; set; }
+    }
+}
---
+++ SinExWebApp20265462/ViewModels/CalculatorShipmentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinExWebApp20265462.ViewModels
+{
+    public class CalculatorShipmentViewModel
+    {
+        public virtual string Origin { get; set; }
+        public virtual string Destination { get; set; }
+        public virtual List<SelectListItem> Destinations { get; set; }
+        public virtual int ServiceTypeID { get; set; }
+        public virtual List<SelectListItem> ServiceTypes { get; set; }
+        
+        public virtual int NumberOfPackages { get; set; }
+        public IList<CalculatorPackageViewModel> Packages { get; set; }
+
+        public virtual string CurrencyCode { get; set; }
+        public virtual List<SelectListItem> CurrencyCodes { get; set; }
+
+        public virtual decimal ShipmentCost { get; set; }
+    }
+}
