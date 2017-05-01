09796e4a37c5eebb02da4ed143c977da59a744d7
wingkeicheung <wkcheungah@connect.ust.hk>
2017-05-02 03:52:28 +0800

    ArrangeShipment ConfirmedShipment GET nearly done.

---
+++ SinExWebApp20265462/Models/Pickup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SinExWebApp20265462.Models
+{
+    [Table("Pickup")]
+    public class Pickup
+    {
+        [Key]
+        public virtual int PickupId { get; set; }
+        public virtual string Type { get; set; } // Immediate or Prearranged
+        public virtual string Address { get; set; }
+        public virtual string City { get; set; }
+        public virtual string Province { get; set; }
+        public virtual DateTime PickupDateTime { get; set; }
+    }
+}
