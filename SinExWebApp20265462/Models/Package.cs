3ab16a7fa6ed54142959e1e880e0f48b6ca6466e
wingkeicheung <wkcheungah@connect.ust.hk>
2017-05-02 03:52:28 +0800

    ArrangeShipment ConfirmedShipment GET nearly done.

---
+++ SinExWebApp20265462/Models/Package.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SinExWebApp20265462.Models
+{
+    [Table("Package")]
+    public class Package
+    {
+        [Key]
+        public virtual int PackageId { get; set; }
+
+        public virtual PackageTypeSize PackageTypeSize { get; set; }
+
+        public virtual string Description { get; set; }
+        public virtual decimal Value { get; set; }
+
+        public virtual float CustomerWeight { get; set; }
+        public virtual float ActualWeight { get; set; }
+
+        public virtual decimal PackageCost { get; set; }
+
+        public virtual Shipment Shipment { get; set; }
+    }
+}
