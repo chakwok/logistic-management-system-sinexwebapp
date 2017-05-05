564a9ad1ba2dae526630067d67ba08f38042d210
pandaec <ericchan1026@gmail.com>
2017-05-05 13:08:43 +0800

    track shipment for demo

---
+++ SinExWebApp20265462/Controllers/ShipmentTrackingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SinExWebApp20265462.Controllers
+{
+    public class ShipmentTrackingController : Controller
+    {
+        // GET: ShipmentTracking
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Details(int? id) {
+            ViewBag.waybill = id;
+            return View();
+        }
+    }
+}
