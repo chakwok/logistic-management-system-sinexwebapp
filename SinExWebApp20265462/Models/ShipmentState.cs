226d64b05b651161eccf0c0e8421bd9e158f4230
pandaec <ericchan1026@gmail.com>
2017-05-08 00:22:22 +0800

    finsih real shipment tracking. complete seed method, now includes packages and states.

---
+++ SinExWebApp20265462/Models/ShipmentState.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace SinExWebApp20265462.Models
+{
+    [Table("ShipmentState")]
+    public class ShipmentState
+    {
+        [Key]
+        public virtual int ShipmentStateID { get; set; }
+
+        public virtual DateTime Time { get; set; }
+        public virtual string Description { get; set; }
+        public virtual string Location { get; set; }
+        public virtual string Remarks { get; set; }
+
+        public virtual Shipment Shipment { get; set; }
+        public virtual int WaybillId { get; set; }
+    }
+}
